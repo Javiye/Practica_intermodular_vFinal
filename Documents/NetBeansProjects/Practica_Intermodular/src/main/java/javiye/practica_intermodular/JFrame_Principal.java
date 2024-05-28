eecce162fc225f7106a3cce40c510c1d3c05a5aa
javiye <jyd0001@alu.medac.es>
2024-05-28 20:37:55 +0200

    Agregadas alertas

--- Documents/NetBeansProjects/Practica_Intermodular/src/main/java/javiye/practica_intermodular/JFrame_Principal.java
+++ Documents/NetBeansProjects/Practica_Intermodular/src/main/java/javiye/practica_intermodular/JFrame_Principal.java
@@ -40,10 +40,10 @@ public class JFrame_Principal extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         Boton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        selectorPlanetas = new javax.swing.JComboBox<>();
         Titulo = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         TablaPlanetas = new javax.swing.JTable();
-        selectorPlanetas = new javax.swing.JComboBox<>();
         jScrollPane2 = new javax.swing.JScrollPane();
         tableSatelite = new javax.swing.JTable();
 
@@ -61,7 +61,7 @@ public class JFrame_Principal extends javax.swing.JFrame {
 
         Distancia.setText("Distancia: ");
 
-        Composicion.setText("Composicion: ");
+        Composicion.setText("Composición: ");
 
         Estrella.setText("Nombre de la estrella: ");
 
@@ -74,6 +74,13 @@ public class JFrame_Principal extends javax.swing.JFrame {
 
         jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\javiy\\Documents\\NetBeansProjects\\Practica_Intermodular\\src\\main\\resources\\images\\sol.tif")); // NOI18N
 
+        selectorPlanetas.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccione un planeta: ", "Tierra", "Marte", "Venus", "Saturno", "Jupiter", "Urano", "Mercurio", "Neptuno", " " }));
+        selectorPlanetas.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                selectorPlanetasActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -81,23 +88,28 @@ public class JFrame_Principal extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(15, 15, 15)
-                        .addComponent(Estrella, javax.swing.GroupLayout.PREFERRED_SIZE, 284, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(15, 15, 15)
+                                .addComponent(Estrella, javax.swing.GroupLayout.PREFERRED_SIZE, 284, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(Boton1)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel1)
+                        .addGap(47, 47, 47)
+                        .addComponent(jLabel4)
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(TipoEstrella, javax.swing.GroupLayout.DEFAULT_SIZE, 332, Short.MAX_VALUE)
+                                .addComponent(Radio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(Temperatura, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(Distancia, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(Composicion, javax.swing.GroupLayout.PREFERRED_SIZE, 424, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(Boton1)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel1)
-                .addGap(47, 47, 47)
-                .addComponent(jLabel4)
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(TipoEstrella, javax.swing.GroupLayout.DEFAULT_SIZE, 332, Short.MAX_VALUE)
-                        .addComponent(Radio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(Temperatura, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(Distancia, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addComponent(Composicion, javax.swing.GroupLayout.PREFERRED_SIZE, 424, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(selectorPlanetas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -121,7 +133,9 @@ public class JFrame_Principal extends javax.swing.JFrame {
                             .addComponent(Distancia)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                             .addComponent(Composicion))))
-                .addContainerGap(25, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(selectorPlanetas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(12, Short.MAX_VALUE))
         );
 
         Titulo.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 18)); // NOI18N
@@ -136,13 +150,6 @@ public class JFrame_Principal extends javax.swing.JFrame {
             }
         ));
         jScrollPane1.setViewportView(TablaPlanetas);
-
-        selectorPlanetas.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "SELECCIONE Planeta", "Tierra", "Marte", "Venus", "Saturno", "Jupiter", "Urano", "Mercurio", "Neptuno", " " }));
-        selectorPlanetas.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                selectorPlanetasActionPerformed(evt);
-            }
-        });
 
         tableSatelite.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -166,36 +173,31 @@ public class JFrame_Principal extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel3)
                     .addComponent(Titulo, javax.swing.GroupLayout.PREFERRED_SIZE, 370, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 631, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(117, Short.MAX_VALUE))
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                     .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1036, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 631, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(selectorPlanetas, javax.swing.GroupLayout.PREFERRED_SIZE, 349, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1036, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addContainerGap()))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(40, 40, 40)
                 .addComponent(Titulo, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel3)
-                .addGap(30, 30, 30)
+                .addGap(64, 64, 64)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(390, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 176, Short.MAX_VALUE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(57, 57, 57))
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
-                    .addGap(299, 299, 299)
-                    .addComponent(selectorPlanetas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGap(12, 12, 12)
+                    .addGap(337, 337, 337)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(71, Short.MAX_VALUE)))
+                    .addContainerGap(221, Short.MAX_VALUE)))
         );
 
         pack();
@@ -203,57 +205,67 @@ public class JFrame_Principal extends javax.swing.JFrame {
 
     private void selectorPlanetasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_selectorPlanetasActionPerformed
 
-// Obtener el nombre del planeta seleccionado
-    String nombrePlaneta = (String) selectorPlanetas.getSelectedItem();
+        // Obtener el nombre del planeta seleccionado
+        String nombrePlaneta = (String) selectorPlanetas.getSelectedItem();
 
-    // Crear una instancia de la clase ConexionBBDD
-    ConexionBBDD conexion = new ConexionBBDD();
+        // Crear una instancia de la clase ConexionBBDD
+        ConexionBBDD conexion = new ConexionBBDD();
 
-    // Obtener los datos del planeta y sus satélites
-    Planeta planeta = conexion.obtenerDatosPlanetaConSatelites(nombrePlaneta);
+        // Obtener los datos del planeta y sus satélites
+        Planeta planeta = conexion.obtenerDatosPlanetaConSatelites(nombrePlaneta);
 
-    // Actualizar los valores en la tabla de planetas
-    TablaPlanetas.setValueAt(planeta.getRadio(), 0, 0);
-    TablaPlanetas.setValueAt(planeta.getDistancia(), 0, 1);
-    TablaPlanetas.setValueAt(planeta.getPeriodo(), 0, 2);
-    TablaPlanetas.setValueAt(planeta.getTipo(), 0, 3);
-    TablaPlanetas.setValueAt(planeta.getTemperatura(), 0, 4);
-    TablaPlanetas.setValueAt(planeta.getNumSatelites(), 0, 5);
-    TablaPlanetas.setValueAt(planeta.getFechaCreacion(), 0, 6);
+        // Actualizar los valores en la tabla de planetas
+        TablaPlanetas.setValueAt(planeta.getRadio(), 0, 0);
+        TablaPlanetas.setValueAt(planeta.getDistancia(), 0, 1);
+        TablaPlanetas.setValueAt(planeta.getPeriodo(), 0, 2);
+        TablaPlanetas.setValueAt(planeta.getTipo(), 0, 3);
+        TablaPlanetas.setValueAt(planeta.getTemperatura(), 0, 4);
+        TablaPlanetas.setValueAt(planeta.getNumSatelites(), 0, 5);
+        TablaPlanetas.setValueAt(planeta.getFechaCreacion(), 0, 6);
 
-    // Limpiar la tabla de satélites
-    for (int i = 0; i < tableSatelite.getRowCount(); i++) {
-        for (int j = 0; j < tableSatelite.getColumnCount(); j++) {
-            tableSatelite.setValueAt("", i, j);
+        // Limpiar la tabla de satélites
+        for (int i = 0; i < tableSatelite.getRowCount(); i++) {
+            for (int j = 0; j < tableSatelite.getColumnCount(); j++) {
+                tableSatelite.setValueAt("", i, j);
+            }
         }
-    }
 
-    // Actualizar los valores en la tabla de satélites si existen
-    int fila = 0;
-    for (Satelite satelite : planeta.getSatelites()) {
-        tableSatelite.setValueAt(satelite.getNombre(), fila, 0);
-        tableSatelite.setValueAt(satelite.getRadio(), fila, 1);
-        tableSatelite.setValueAt(satelite.getDistancia(), fila, 2);
-        tableSatelite.setValueAt(satelite.getPeriodo(), fila, 3);
-        tableSatelite.setValueAt(satelite.getTemperatura(), fila, 4);
-        tableSatelite.setValueAt(satelite.getTipo(), fila, 5);
-        tableSatelite.setValueAt(satelite.getFechaCreacion(), fila, 6);
-        fila++;
-    }
+        // Actualizar los valores en la tabla de satélites si existen
+        int fila = 0;
+        for (Satelite satelite : planeta.getSatelites()) {
+            tableSatelite.setValueAt(satelite.getNombre(), fila, 0);
+            tableSatelite.setValueAt(satelite.getRadio(), fila, 1);
+            tableSatelite.setValueAt(satelite.getDistancia(), fila, 2);
+            tableSatelite.setValueAt(satelite.getPeriodo(), fila, 3);
+            tableSatelite.setValueAt(satelite.getTemperatura(), fila, 4);
+            tableSatelite.setValueAt(satelite.getTipo(), fila, 5);
+            tableSatelite.setValueAt(satelite.getFechaCreacion(), fila, 6);
+            fila++;
+        }
 
-    // Mostrar la tabla de satélites si tiene datos
-    tableSatelite.setVisible(fila > 0);
-    //tableSatelite.setVisible(false);
+        // Mostrar la tabla de satélites si tiene datos
+        tableSatelite.setVisible(fila > 0);
+
+        // Mostrar mensaje de alerta con el número de satélites
+        String numSatelites = planeta.getNumSatelites();
+        
+        if (nombrePlaneta.equalsIgnoreCase("Seleccione un planeta: ")) {
+            return; // Salir del método si no se ha seleccionado un planeta válido
+        } else {
+            JOptionPane.showMessageDialog(null, "El planeta " + nombrePlaneta + " tiene " + numSatelites + " satélites.");
+
+            //tableSatelite.setVisible(false);
+        }
+
 
     }//GEN-LAST:event_selectorPlanetasActionPerformed
 
     private void Boton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Boton1ActionPerformed
-        
+
         /* YA QUE SÓLO EXISTE UNA ESTRELLA Y EL ENUNCIADO ESPECIFICA QUE ESA INFORMACIÓN ES FIJA
         En vez de crear una clase con sus respectivos métodos, concateno la información que ya existía en JLABEL para mostrar la información del sol.
         
          */
-        
         // Verificar si el botón ya ha sido presionado
         boolean botonPresionado = Boton1.isSelected();
 
@@ -276,16 +288,15 @@ public class JFrame_Principal extends javax.swing.JFrame {
 
             String composicionActual = Composicion.getText();
             Composicion.setText(composicionActual + " 74% Hidrógeno, 24% Helio, 2% Otros");
-            
-            JOptionPane.showMessageDialog(this, "EL ENUNCIADO ESPECIFICA QUE ESA INFORMACIÓN ES FIJA."); 
 
+            JOptionPane.showMessageDialog(this, "EL ENUNCIADO ESPECIFICA QUE ESA INFORMACIÓN ES FIJA.");
 
             // Deshabilitar el botón
             Boton1.setEnabled(false);
             botonPresionado = true;
         } else {
             // El botón ya ha sido presionado, no hacer nada
-            JOptionPane.showMessageDialog(this, "El botón ya ha sido presionado..."); 
+            JOptionPane.showMessageDialog(this, "El botón ya ha sido presionado...");
             //No debería en teoría tener opción a este mensaje...
         }
     }//GEN-LAST:event_Boton1ActionPerformed
